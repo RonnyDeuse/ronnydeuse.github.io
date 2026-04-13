4ee03c6ed17b5ce442c5de76e000d4762379f4eb
Ronny Deuse <ronny.deuse@gmail.com>
2026-04-13 19:32:24 +0200

    use nicer looking font

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import './style.css'
+import './main.css'
 
-document.querySelector('#app').innerHTML = ``;
+document.querySelector('#app').innerHTML = `
+    <section>
+        <p>
+            My personal GitHub page.
+            A brief job profile and a few private project infos.
+        </p>
+    </section>
+`;
 
 console.log("Application started");
