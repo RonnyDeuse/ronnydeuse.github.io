66b9f6e0d196ce8ca77a4365d5639889641711a4
Ronny Deuse <ronny.deuse@gmail.com>
2026-04-13 19:24:34 +0200

    made the page a bit more responsive

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # ronnydeuse.github.io
 
 My personal GitHub page.
+A brief job profile and a few private project infos.
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = ``;
+
+console.log("Application started");
